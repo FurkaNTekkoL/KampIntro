edb0cf574d1c92c2721c84542850ff26c7a9fd28
furkan tekkol <tekkol.furkan@gmail.com>
2021-09-12 17:20:17 +0300

    son

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added!");
+        }
+
+        public void Listed(Customer customer)
+        {
+            Console.WriteLine("Customer Listed!");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted!");
+        }
+    }
+}
