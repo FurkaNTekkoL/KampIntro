d0168445d5b3dff3e4dbce703787d7ff95d0dcb0
furkan tekkol <tekkol.furkan@gmail.com>
2021-09-12 22:54:14 +0300

    son

---
+++ Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            //names = new string[5];
+            //names[4] = "İlker";
+            //Console.WriteLine(names[4]);
+            //Console.WriteLine(names[0]);     // Yeniden new 'lendiği için farklı bir referans oluştu ve eski dizi elemanlarını kaybetti.
+            //                                 // Gelen dataya yeni bir eleman eklenemiyor.Bu yüzden dizi yerine çoğunlukla koleksiyonlar kullanılır.
+
+            List<string> names2 = new List<string> {"Engin","Murat","Kerem","Halil" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("İlker");               // Koleksiyonların Add metodunu kullandık.
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+        }
+    }
+}
---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()                     // MyList class'ını bir yerde new'lersek constructor bloğu otomatik çalışır. 0 elemanlı bir array yaptık.
+        {
+            items = new T[0];
+        }
+        public void Add(T item)             // Bu metoda T tipinde elemanlar eklemek için new'lenmesi gerekiyor.Contructor ile bunu yapmış olduk.
+        {
+            T[] tempArray = items;          // New' lendiğinde eski referansımızın kaybolmaması için "Geçici Dizi" oluşturup eski referansı atadık.
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];    // TempArray 'e atadığımız eski referansın elemanlarını geri çağırdık.
+            }
+
+            items[items.Length - 1] = item;  // Anlamı: items'ın sonuncu(items.length-1) indexi = gönderdiğimiz item.
+        }
+    }
+}
